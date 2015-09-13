dd5d2a4d2f34dfa71dc17bba3b0ecc4569e42e77
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2015-09-13 21:51:25 +0200

    MIPS: add initial infrastructure for Intel XWAY SoCs
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ arch/mips/mach-intel/include/mach/io.h
@@ -0,0 +1,10 @@
+/*
+ * Copyright (C) 2016 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __MACH_INTEL_IO_H
+#define __MACH_INTEL_IO_H
+
+#endif /* __MACH_INTEL_IO_H */
---
+++ arch/mips/mach-intel/include/mach/soc.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (C) 2016 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __MACH_INTEL_SOC_H
+#define __MACH_INTEL_SOC_H
+
+enum intel_xway_partnum {
+	INTEL_XWAY_UNKNOWN = 0,
+};
+
+#endif /* __MACH_INTEL_SOC_H */
