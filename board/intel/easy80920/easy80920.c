49ddd5beffb0512a1ccaf3d4a73b21acc542de9a
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2016-01-14 21:39:55 +0100

    MIPS: add Intel EASY80920 evaluation board
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ board/intel/easy80920/easy80920.c
@@ -0,0 +1,8 @@
+/*
+ * Copyright (C) 2016 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <common.h>
+
---
+++ include/configs/easy80920.h
@@ -0,0 +1,15 @@
+/*
+ * Copyright (C) 2016 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#include <configs/intel_xway_common.h>
+#include <configs/intel_xway_vrx200.h>
+
+#define CONFIG_ENV_IS_NOWHERE
+#define CONFIG_ENV_SIZE			(8 * 1024)
+
+#define CONFIG_SYS_LONGHELP
+#define CONFIG_AUTO_COMPLETE
+#define CONFIG_CMDLINE_EDITING
