2371b6b26f055e2be279cdab512218a63488e743
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2016-01-14 21:39:42 +0100

    MIPS: add support for Intel XWAY VRX200 SoC family
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ include/configs/intel_xway_vrx200.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2016 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_INTEL_XWAY_VRX200_H
+#define __CONFIG_INTEL_XWAY_VRX200_H
+
+/* CPU */
+#define CONFIG_SYS_MIPS_TIMER_FREQ	250000000
+
+/* RAM */
+#define CONFIG_NR_DRAM_BANKS		1
+#define CONFIG_SYS_SDRAM_BASE		0x80000000
+
+/* SRAM */
+#define CONFIG_SYS_SRAM_BASE		0xbe220000
+#define CONFIG_SYS_SRAM_SIZE		0x10000
+
+/* U-Boot */
+#define CONFIG_SYS_LOAD_ADDR		0x80100000
+
+#if defined(CONFIG_INTEL_BOOT_RAM)
+#define CONFIG_SYS_INIT_SP_ADDR		0x80010000
+#define CONFIG_SKIP_LOWLEVEL_INIT
+#endif
+
+#endif /* __CONFIG_INTEL_XWAY_VRX200_H */
