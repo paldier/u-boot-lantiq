5845298752386f1b21c0d2fd9c2f67e03a28a669
Daniel Schwierzeck <daniel.schwierzeck@gmail.com>
2016-01-21 14:59:03 +0100

    MIPS: add support for Intel XWAY GRX330 SoC family
    Signed-off-by: Daniel Schwierzeck <daniel.schwierzeck@gmail.com>

---
+++ include/configs/intel_xway_grx330.h
@@ -0,0 +1,29 @@
+/*
+ * Copyright (C) 2016 Daniel Schwierzeck, daniel.schwierzeck@gmail.com
+ *
+ * SPDX-License-Identifier:	GPL-2.0+
+ */
+
+#ifndef __CONFIG_INTEL_XWAY_GRX330_H
+#define __CONFIG_INTEL_XWAY_GRX330_H
+
+/* CPU */
+#define CONFIG_SYS_MIPS_TIMER_FREQ	300000000
+
+/* RAM */
+#define CONFIG_NR_DRAM_BANKS		1
+#define CONFIG_SYS_SDRAM_BASE		0x80000000
+
+/* SRAM */
+#define CONFIG_SYS_SRAM_BASE		0xbe1a0000
+#define CONFIG_SYS_SRAM_SIZE		0x10000
+
+/* U-Boot */
+#define CONFIG_SYS_LOAD_ADDR		0x80100000
+
+#if defined(CONFIG_INTEL_BOOT_RAM)
+#define CONFIG_SYS_INIT_SP_ADDR		0x80010000
+#define CONFIG_SKIP_LOWLEVEL_INIT
+#endif
+
+#endif /* __CONFIG_INTEL_XWAY_GRX330_H */
